fa29697a16fab27776e07952fc77fbffd150428c
Oleksandr <103380042+Tsymbaliuk-Oleksandr@users.noreply.github.com>
2022-11-13 20:57:20 +0200

    goit-react-hw-01-components(end)

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
-import { FriendListItem } from './FriendListItem';
+import css from '../FriendList/FriendList.module.css'
+import { FriendListItem } from '../FriendListItem/FriendListItem';
 
 export const FriendList = ({
   friends,
 }) => {
   return (
-<ul className="friend-list">
+<ul className={css.friendList}>
   {/* <!-- Довільна кіл-сть FriendListItem --> */}
 
   {friends.map(({id, isOnline, avatar, name}) => {
