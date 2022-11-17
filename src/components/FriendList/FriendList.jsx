794c7f3bb18c825500bea85a03c37aa252708bf0
Oleksandr <103380042+Tsymbaliuk-Oleksandr@users.noreply.github.com>
2022-11-17 22:10:33 +0200

    fixed code (end)

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,25 +1,31 @@
-import PropTypes from 'prop-types';
-import css from '../FriendList/FriendList.module.css'
-import { FriendListItem } from '../FriendList/FriendListItem';
+import PropTypes from "prop-types";
+import css from "../FriendList/FriendList.module.css";
+import { FriendListItem } from "../FriendList/FriendListItem";
 
-export const FriendList = ({
-  friends,
-}) => {
+export const FriendList = ({ friends }) => {
   return (
-<ul className={css.friendList}>
-
-  {friends.map(({id, isOnline, avatar, name}) => {
-    return <FriendListItem key={id} isOnline={isOnline} avatar={avatar} name={name}/>
-  })}
-</ul>
+    <ul className={css.friendList}>
+      {friends.map(({ id, isOnline, avatar, name }) => {
+        return (
+          <FriendListItem
+            key={id}
+            isOnline={isOnline}
+            avatar={avatar}
+            name={name}
+          />
+        );
+      })}
+    </ul>
   );
 };
 
 FriendList.propTypes = {
-  friends: PropTypes.arrayOf(PropTypes.exact({
-    id: PropTypes.number.isRequired,
-    avatar: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    isOnline: PropTypes.bool.isRequired,
-  }))
+  friends: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ),
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,5 @@
-import PropTypes from 'prop-types';
-import css from '../Profile/Profile.module.css'
+import PropTypes from "prop-types";
+import css from "../Profile/Profile.module.css";
 
 export const Profile = ({
   username,
@@ -9,34 +9,34 @@ export const Profile = ({
   stats: { followers, views, likes },
 }) => {
   return (
-<div className={css.profile}>
-  <div className={css.description}>
-    <img
-      src={avatar}
-      alt="User avatar"
-      className={css.avatar}
-      width='100'
-    />
-    <p className={css.name}>{username}</p>
-    <p className={css.tag}>@{tag}</p>
-    <p className={css.location}>{location}</p>
-  </div>
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img
+          src={avatar}
+          alt="User avatar"
+          className={css.avatar}
+          width="100"
+        />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
 
-  <ul className={css.stats}>
-    <li className={css.statsposition}>
-      <span className={css.label}>Followers</span>
-      <span className={css.quantity}>{followers}</span>
-    </li>
-    <li className={css.statsposition}>
-      <span className={css.label}>Views</span>
-      <span className={css.quantity}>{views}</span>
-    </li>
-    <li className={css.statsposition}>
-      <span className={css.label}>Likes</span>
-      <span className={css.quantity}>{likes}</span>
-    </li>
-  </ul>
-</div>
+      <ul className={css.stats}>
+        <li className={css.statsposition}>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{followers}</span>
+        </li>
+        <li className={css.statsposition}>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{views}</span>
+        </li>
+        <li className={css.statsposition}>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{likes}</span>
+        </li>
+      </ul>
+    </div>
   );
 };
 
